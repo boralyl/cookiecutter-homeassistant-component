006fe6993f6a98af1ce850eae3931cb4c7add14d
Aaron Godfrey <me@aarongodfrey.dev>
2020-05-31 14:46:25 -0700

    Initial commit.

---
+++ {{ cookiecutter.domain }}/custom_components/{{ cookiecutter.domain }}/__init__.py
@@ -0,0 +1,7 @@
+from homeassistant import core
+
+
+async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
+    """Set up the {{ cookiecutter.name }} component."""
+    # @TODO: Add setup code.
+    return True
