4a827eafc1f5991640707b15b245cb3655a2b3f2
Aaron Godfrey <me@aarongodfrey.dev>
2021-05-12 09:29:25 -0700

    Update post_gen_project.py
    Skip post gen hook on windows platforms.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,11 @@
 import os
+import platform
 
-# Rename root repo directory to use `-` instead of `_`.
-repo_dir = "{{cookiecutter.domain}}"
-new_repo_dir = repo_dir.replace("_", "-")
-os.rename(os.path.join("..", repo_dir), os.path.join("..", new_repo_dir))
+
+# Skipping this operation on windows since this is more cosmetic and
+# causes a PermissionError on that platform.
+if platform.system() != "Windows":
+    # Rename root repo directory to use `-` instead of `_`.
+    repo_dir = "{{cookiecutter.domain}}"
+    new_repo_dir = repo_dir.replace("_", "-")
+    os.rename(os.path.join("..", repo_dir), os.path.join("..", new_repo_dir))
